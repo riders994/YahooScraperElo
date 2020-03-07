d950ab5845fb73833a68e7e6b9d0007939cd3c76
Rohan Vahalia <r.vahalia@gmail.com>
2020-03-07 09:14:17 -0600

    Requirements and setup.py cleanup

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version='2.0',
     packages=find_packages(exclude=['tests']),
     install_requires=[
-        'objectpath', 'pytz', 'yahoo_oauth', 'docopt', 'yahoo_fantasy_api',
-        'YahooScrapingTools @ git+https://github.com/riders994/YahooScrapingTools.git@v1'
+        'pandas==1.0.0',
+        'YahooScrapingTools @ git+https://github.com/riders994/YahooScrapingTools.git@v1',
     ],
     python_requires='>=3',
 )
